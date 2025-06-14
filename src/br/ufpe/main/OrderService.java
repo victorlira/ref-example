e5d04f19e506c39fa25668ad0b98e20450df608e
antonio <antonio.rec@gmail.com>
2025-06-14 12:34:56 -0300

    real-base-commit

--- src/br/ufpe/main/OrderService.java
+++ src/br/ufpe/main/OrderService.java
@@ -3,7 +3,7 @@ package br.ufpe.main;
 class OrderService {
 	double calcFinalPrice(boolean cash, double price) {
 		double finalPrice = cash ? price : price * 0.9;
-		if (price > 100) {
+		if (finalPrice > 100) {
 			finalPrice = price + (price * 0.1);
 		}
 		return finalPrice;
