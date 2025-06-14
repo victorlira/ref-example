0f150fb74b072d95199035fe90cefbd13b159c6e
antonio <antonio.rec@gmail.com>
2025-06-14 13:26:58 -0300

    new base

--- src/br/ufpe/main/OrderService.java
+++ src/br/ufpe/main/OrderService.java
@@ -2,13 +2,13 @@ package br.ufpe.main;
 
 class OrderService {
 	double calcFinalPrice(boolean cash, double price) {
-		double finalPrice = cash ? price : price * 0.95;
+		double finalPrice = price;
+		if (cash) {
+			finalPrice = finalPrice * 0.9;
+		}
 		if (finalPrice > 100) {
-			finalPrice = applyTax(finalPrice);
+			finalPrice = finalPrice + (finalPrice * 0.1);
 		}
 		return finalPrice;
-	}
-	double applyTax(double value) {
-		return value + (value * 0.1);
 	}
 }
